cf6f285045a6e9e167e2ea1f8d41f1867e78366c
oussema hidri <oussama.hidri01@gmail.com>
2023-03-28 09:38:01 +0100

    checkpoint react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import Carousels from "./Component/Carousels"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Oussema Hidri</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Portfolio</Nav.Link>
+                            <Nav.Link href="#link">Resume</Nav.Link>
+                            <Nav.Link href="#link">About</Nav.Link>
+                            <Nav.Link href="#link">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
+            <Carousels/>
+        </div>
+    );
 }
 
 export default App;
